81a1639e639cc717417062a662d833abc930646e
wy <wy@aokodeiMac.local>
2019-07-25 18:14:37 +0800

    添加api文档显示示例

--- application/api/controller/Auth.php
+++ application/api/controller/Auth.php
@@ -16,11 +16,20 @@ class Auth
 {
     /**
      * @doc('创建授权')
-     * @route('','get')
+     * @route('','post')
      * @validate('Token')
      * @param('name','名称','require')
      * @param('password','密码','require')
      * @return \think\response\Json
+     * @success('{
+          "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUUlIiLCJpYXQiOjE1NjQwNDc4ODcsImV4cCI6MTU2NDA1NTA4NywidXVpZCI6MTAwLCJzaWduYXR1cmUiOiIxMiJ9.QAvjERUOvQ2QwUcPnQOJuYGuTDgzWCZ7gaNziJHDmVI",
+          "refresh_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUUlIiLCJpYXQiOjE1NjQwNDc4ODcsInV1aWQiOjEwMCwic2lnbmF0dXJlIjoiMTIifQ.n-TZSFr9NqaTIjWpxR3ZUeP7WobYrhYvS5lIVkxRaIM"
+          }')
+     * @error('{
+          "code": 3000,
+          "message": "3000: 错误内容 . 参数验证 .   name不能为空,password不能为空",
+          "request_url": "auth"
+      }')
      */
     public function create()
     {
@@ -33,6 +42,7 @@ class Auth
      * @return array
      * @throws \app\lib\exception\token\TokenException
      * @throws \think\Exception
+     * @success('')
      */
     public function refresh()
     {
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,43 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends Controller
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
+        $parser = new \HyperDown\Parser;
+        $html = $parser->makeHtml(file_get_contents(env('ROOT_PATH').'api-md.md'));
+//        $Extra = new \Parsedown();
+//        $html = $Extra->parse(file_get_contents(env('ROOT_PATH').'api-md.md'));
+//        dump($html);die;
+        return $this->display(
+            "<html>
+                        <header>
+                            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+                            <link href=\"https://cdn.bootcss.com/github-markdown-css/3.0.1/github-markdown.css\" rel=\"stylesheet\">
+                            <style>
+                                .markdown-body {
+                                    box-sizing: border-box;
+                                    min-width: 200px;
+                                    max-width: 980px;
+                                    margin: 0 auto;
+                                    padding: 45px;
+                                }
+                            
+                                @media (max-width: 767px) {
+                                    .markdown-body {
+                                        padding: 15px;
+                                    }
+                                }
+                            </style>
+                        </header>
+                        <body class='markdown-body'>$html<body>
+                     </html>"
+        );
+
+
     }
 
     public function hello($name = 'ThinkPHP5')
