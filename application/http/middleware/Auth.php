c9d108e14aba44d9667e1a0cad844289eaa2d762
高永立 <phperali@gmail.com>
2019-08-16 11:27:20 +0800

    鉴权验证中间件编写

--- application/http/middleware/Auth.php
+++ application/http/middleware/Auth.php
@@ -1,8 +1,10 @@
 <?php
 /** Created By wene<china_wangyu@aliyun.com>, Data: 2019/7/3 */
 
-
 namespace app\http\middleware;
+
+
+use app\lib\token\Token;
 
 class Auth
 {
@@ -14,10 +16,10 @@ class Auth
      */
     public function handle($request, \Closure $next)
     {
-        if (request()->controller() != 'Auth'){
-
+        if (!empty($request->routeInfo()['option']['middleware'])
+            && in_array('Auth', $request->routeInfo()['option']['middleware'])) {
+            Token::verification();
         }
-
         return $next($request);
     }
 }
